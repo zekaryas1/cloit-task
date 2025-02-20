f263b0114e1eb6d8ed42caf846b062817d8c079e
Zekaryas Tadele <zekarasdinku@gmail.com>
2025-02-20 15:32:58 +0300

    convert to public variables

--- cloit-frontend/utils/variables.ts
+++ cloit-frontend/utils/variables.ts
@@ -1,2 +1,2 @@
-export const API_URL = process.env.API_URL || "http://localhost:4000"
-export const FRONTEND_API_URL = process.env.FRONTEND_API_URL || "http://localhost:3000"
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000"
+export const FRONTEND_API_URL = process.env.NEXT_PUBLIC_FRONTEND_API_URL || "http://localhost:3000"
